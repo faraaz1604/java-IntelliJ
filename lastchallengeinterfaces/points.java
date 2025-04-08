454065b6506d79c9a6bafd38b48c91e6b587c249
Faraaz <mohamedfaraaz007@gmail.com>
2025-04-08 15:55:15 +0530

    init commit

--- lastchallengeinterfaces/points.java
+++ lastchallengeinterfaces/points.java
@@ -4,4 +4,25 @@ public class points {
 
     private int points;
     private int TotalPoints;
+
+    public points(int points, int totalPoints) {
+        this.points = points;
+        TotalPoints = totalPoints;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getTotalPoints() {
+        return TotalPoints;
+    }
+
+    public void setTotalPoints(int totalPoints) {
+        TotalPoints = totalPoints;
+    }
 }
