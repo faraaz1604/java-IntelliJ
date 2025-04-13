8d095803449e0941485e024beeadfc99b80ecc66
Faraaz <mohamedfaraaz007@gmail.com>
2025-04-13 11:44:49 +0530

    Predicate and all combine functional intefaces

---
+++ FunctinalIterfaces/Combinefunctions.java
@@ -0,0 +1,39 @@
+package FunctinalIterfaces;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class Combinefunctions {
+
+
+    public static void main(String[] args) {
+
+        Supplier<List<Integer>> number = () -> {
+            List<Integer> numGen = new ArrayList<>(10);
+            Random random = new Random();
+            for (int i = 0; i<10; i++){
+                numGen.add(random.nextInt(100)+1);
+            }
+            return numGen;
+        };
+
+        Predicate<Integer> isOdd = num -> num % 2 !=0;
+
+        Function<Integer, String> convertString = n -> "Number: " + n;
+
+        Consumer<String> printNum = s -> System.out.println(s);
+
+        List<Integer> numbergenerated = number.get();
+        System.out.println(numbergenerated);
+
+        numbergenerated.stream()
+                .filter(isOdd)
+                .map(convertString)
+                .forEach(printNum);
+    }
+}
--- FunctinalIterfaces/PredicateMission.java
+++ FunctinalIterfaces/PredicateMission.java
@@ -1,12 +1,40 @@
 package FunctinalIterfaces;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class PredicateMission {
     public static void main(String[] args) {
-        Predicate<Integer> isEven = num -> num%2==0;
+//        Predicate<Integer> isEven = num -> num%2==0;
+//
+//        System.out.println("Is 20 an even number? " + isEven.test(20));
+//        System.out.println("Is 15 an even number? " + isEven.test(15));
 
-        System.out.println("Is 20 an even number? " + isEven.test(20));
-        System.out.println("Is 15 an even number? " + isEven.test(15));
+        Predicate<String> isValidEmail = email ->
+                email != null && email.contains("@") && email.contains(".");
+        boolean valid = isValidEmail.test("user@example.com"); // true
+
+        Predicate<Integer> isEven = num -> num % 2 == 0;
+        Predicate<Integer> integerPredicate = num -> num > 50;
+
+        Predicate<Integer> combine = isEven.and(integerPredicate);
+
+        System.out.println(combine.test(60));
+
+
+        Predicate<List<String>> isEmpty = list -> list.isEmpty();
+        Predicate<List<String>> isNotEmpty =  isEmpty.negate();
+
+        List<String> emptyList = new ArrayList<>();
+        List<String> fullList = Arrays.asList("item1", "item2");
+
+        boolean result1 = isNotEmpty.test(emptyList); // false
+        boolean result2 = isNotEmpty.test(fullList);
+
+        System.out.println(result1);
+        System.out.println(result2);
+
     }
 }
