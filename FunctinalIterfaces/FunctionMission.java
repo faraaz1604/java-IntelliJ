4c9ba901e387f60b394516f7ead3d75a309f021b
Faraaz <mohamedfaraaz007@gmail.com>
2025-04-13 10:48:31 +0530

    Function Interface Logic added

--- FunctinalIterfaces/FunctionMission.java
+++ FunctinalIterfaces/FunctionMission.java
@@ -1,5 +1,9 @@
 package FunctinalIterfaces;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 public class FunctionMission {
@@ -8,6 +12,40 @@ public class FunctionMission {
 
         System.out.println("UpperCase of 'faraaz' is: " + wordToUpperCase.apply("faraaz"));
 
+
+        Function<String, String> reverseString = s -> {
+            String reverse = "";
+            for (int i = 0; i<s.length(); i++){
+                reverse = s.charAt(i) + reverse;
+            }
+            return reverse;
+        };
+
+        System.out.println(reverseString.apply("Faraaz"));
+
+        Function<List<String>, Map<String , Integer>> typeConversion = list -> {
+
+            Map<String, Integer> map = new HashMap<>();
+
+            for (String s : list){
+                map.put(s , s.length());
+            }
+            return map;
+
+        };
+
+        List<String> words = Arrays.asList("apple", "banana", "cherry");
+        Map<String, Integer> map = typeConversion.apply(words);
+        System.out.println(map);
+
+
+        Function<Integer, Integer> add10 = num -> num+10;
+        Function<Integer, Integer> mul2 = num -> num*2;
+        Function<Integer, String> combinetoString = n -> "result: "+ n;
+
+        Function<Integer, String> processNumber = add10.andThen(mul2).andThen(combinetoString);
+
+        System.out.println(processNumber.apply(5));
 
     }
 }
