361d74e3cbe7427b2ab5241728614b1b9ca9f9bd
Faraaz <mohamedfaraaz007@gmail.com>
2025-04-19 11:48:03 +0530

    Added new java UnCheckedExample2 class

---
+++ Excepion/UncheckedExample2.java
@@ -0,0 +1,8 @@
+package Excepion;
+
+public class UncheckedExample2 {
+    public static void main(String[] args) {
+        String name = null;
+        System.out.println(name.length());
+    }
+}
