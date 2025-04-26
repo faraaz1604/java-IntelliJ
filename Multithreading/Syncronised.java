6a3cc1d663bbb4651c5a3fd71a816a24e444c741
Faraaz <mohamedfaraaz007@gmail.com>
2025-04-26 13:14:12 +0530

    synchronization Challenge added

---
+++ Multithreading/Syncronised.java
@@ -0,0 +1,40 @@
+package Multithreading;
+
+public class Syncronised {
+
+    int counter;
+
+    public synchronized void increment(){
+        counter++;
+    }
+
+
+    public int getCounter(){
+        return counter;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Syncronised sy = new Syncronised();
+
+        Thread thread1 = new Thread(() ->{
+            for (int i =1; i<=1000; i++){
+                 sy.increment();
+            }
+        });
+
+        Thread thread2 = new Thread(() ->{
+            for (int i =1; i<=1000; i++){
+                sy.increment();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println(sy.getCounter());
+    }
+}
