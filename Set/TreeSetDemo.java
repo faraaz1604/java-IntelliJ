4a857e04b0b3c64c8ebd485b3908921a8a9ebdd3
Faraaz <mohamedfaraaz007@gmail.com>
2025-05-07 12:01:49 +0530

    TreeSetDemo imp methods

--- Set/TreeSetDemo.java
+++ Set/TreeSetDemo.java
@@ -13,8 +13,19 @@ public class TreeSetDemo {
         set.add(80);
         set.add(70);
         set.add(20);
-
         System.out.println(set);
+
+        for (int n : set){
+            System.out.println(n);
+        }
+
+        System.out.println(set.contains(90));
+        System.out.println(set.size());
+        System.out.println(set.remove(90));
+        System.out.println(set);
+        set.clear();
+        System.out.println(set.isEmpty());
+
 
 
     }
