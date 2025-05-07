e8eefe1283273f1dae3917bfe9e08905e8d28b8a
Faraaz <mohamedfaraaz007@gmail.com>
2025-05-07 08:06:48 +0530

    TreeSetDemo added

---
+++ Set/TreeSetDemo.java
@@ -0,0 +1,21 @@
+package Set;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+    public static void main(String[] args) {
+
+        Set<Integer> set =  new TreeSet<>();
+        set.add(90);
+        set.add(10);
+        set.add(80);
+        set.add(70);
+        set.add(20);
+
+        System.out.println(set);
+
+
+    }
+}
