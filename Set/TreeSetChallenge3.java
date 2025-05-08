e2e96e26ed120890e26c1b4b9961d9e3f85fe33b
Faraaz <mohamedfaraaz007@gmail.com>
2025-05-08 21:50:07 +0530

    TreeSetChallenge3

---
+++ Set/TreeSetChallenge3.java
@@ -0,0 +1,33 @@
+package Set;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetChallenge3 {
+
+    public static int findClosest(int[] arr, int target){
+
+        TreeSet<Integer> nums = new TreeSet<>();
+
+        for (int n : arr){
+            nums.add(n);
+        }
+
+        Integer floor = nums.floor(target);
+        Integer ceiling = nums.ceiling(target);
+
+        System.out.println(floor);
+        System.out.println(ceiling);
+
+        if (floor == null)return ceiling;
+        if (ceiling == null)return floor;
+
+        return (Math.abs(target-floor)<= Math.abs(ceiling-target))?floor:ceiling;
+    }
+
+    public static void main(String[] args) {
+        int[] sortedArr = {1, 3, 5, 7, 9};
+        int target = 5;
+        System.out.println("Closest: " + findClosest(sortedArr, target)); // Output: 5 or 7 (depending on logic)
+    }
+}
